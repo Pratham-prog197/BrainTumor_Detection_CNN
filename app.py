f93aeca87e07997051c2bcf3f452e3117bee2567
Pratham-prog197 <prathamthesingh1@gmail.com>
2026-03-17 14:25:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,16 +7,11 @@ import gdown
 import os
 from utils import preprocess_image, postprocess_mask
 
-# =====================
-# CONFIG
-# =====================
 IMG_SIZE = 128
 MODEL_URL = "https://drive.google.com/uc?id=1rq6N83xcAzmxYOvMGQzkMOO9W6gSZ3AB"
 MODEL_PATH = "brain_tumor_unet.h5"
 
-# =====================
-# DOWNLOAD MODEL (ONCE)
-# =====================
+
 @st.cache_resource
 def load_model():
     if not os.path.exists(MODEL_PATH):
@@ -24,22 +19,12 @@ def load_model():
     model = tf.keras.models.load_model(MODEL_PATH, compile=False)
     return model
 
+
 model = load_model()
 
-# =====================
-# PREPROCESS FUNCTION
-# =====================
-def preprocess_image(image):
-    image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
-    image = image / 255.0
-    return image.reshape(1, IMG_SIZE, IMG_SIZE, 3)
-
-# =====================
-# STREAMLIT UI
-# =====================
 st.title("🧠 Brain Tumor Segmentation using U-Net")
 
-uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
 
@@ -49,11 +34,17 @@ if uploaded_file is not None:
     st.subheader("Uploaded MRI")
     st.image(img, use_column_width=True)
 
-input_img = preprocess_image(img)
-pred = model.predict(input_img)
-mask = postprocess_mask(pred)
+    input_img = preprocess_image(img)
+
+    with st.spinner("Predicting Tumor Region..."):
+        pred = model.predict(input_img)
+
+    mask = postprocess_mask(pred)
 
     st.subheader("Predicted Tumor Mask")
-    plt.imshow(mask, cmap="gray")
-    plt.axis("off")
-    st.pyplot(plt)
+
+    fig, ax = plt.subplots()
+    ax.imshow(mask, cmap="gray")
+    ax.axis("off")
+
+    st.pyplot(fig)
